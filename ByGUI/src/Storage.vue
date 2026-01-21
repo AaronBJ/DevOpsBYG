d4db37c2ff354aba5c489de374c1f532626f6741
AARON BECERRA <aaronbecerrajuarez@gmail.com>
2026-01-21 13:41:43 -0700

    se agrego la vista de la tabla

--- ByGUI/src/Storage.vue
+++ ByGUI/src/Storage.vue
@@ -2,71 +2,89 @@
 </script>
 
 <template>
-    <div class="container">
+  <div class="container">
 
-      <div class="row">
-        <div class="col-6 offset-3" >
-          <div class="input-group">
-            <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
-            <input class="form-control" type="search" placeholder="Busqueda" id="gsearch" name="gsearch">
-          </div>
-        </div>
-
-        <div class="col offset-1">
-          <input type="button" class="btn btn-primary" value="Agregar">
+    <div class="row">
+      <div class="col-6 offset-3">
+        <div class="input-group">
+          <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+          <input class="form-control" type="search" placeholder="Busqueda" id="gsearch" name="gsearch">
         </div>
       </div>
 
-      <div class="row">
-
-        <label for="id">ID</label>
-        &nbsp;
-        <label for="desc">DESCRIPCION</label>
-        &nbsp;
-        <label for="cant">CANTIDAD</label>
-        &nbsp;
-        <label for="img">IMAGEN</label>
-        <hr>
-
+      <div class="col offset-1">
+        <input type="button" class="btn btn-primary" value="Agregar">
       </div>
+    </div>
 
-      <div class="row">
 
-        <input type="number" id="id" name="id" />
-        &nbsp;
-        <input type="text" id="desc" name="desc" />
-        &nbsp;
-        <input type="number" id="cant" name="cant" />
-        &nbsp;
-        <input type="image" />
-        &nbsp;
-        <input type="button" value="Detalles" />
-        <br />
-        <input type="button" value="Editar" />
-        &nbsp;
-        <input type="button" value="Eliminar" />
-        <hr />
+    <div class="row">
+      <table class="table table-hover">
+        <thead >
+          <tr>
+            <th> ID </th>
+            <th>DESCRIPCION</th>
+            <th>CANTIDAD</th>
+            <th>IMAGEN</th>
+            <th>DETALLES</th>
+          </tr>
+        </thead>
 
-      </div>
+        <tbody>
+          <tr>
+            <td> 1 </td>
+            <td> BRK 356 </td>
+            <td> 12 </td>
+            <td> :) </td>
+            <td>
+              <input type="button" value="i" />
+            </td>
+          </tr>
 
-      <div class="row">
-        <input type="button" value="Ver todos" />
-      </div>
+          <tr>
+            <td> 1 </td>
+            <td> BRK 356 </td>
+            <td> 12 </td>
+            <td> :) </td>
+            <td>
+              <input type="button" value="i" />
+            </td>
+          </tr>
 
-      <div class="row">
-        <label for="nregis">numero de registros por pagina:</label>
-      </div>
+          <tr>
+            <td> 1 </td>
+            <td> BRK 356 </td>
+            <td> 12 </td>
+            <td> :) </td>
+            <td>
+              <input type="button" value="i" />
+            </td>
+          </tr>
+        </tbody>
 
-      <div class="row">
-        <input type="number" id="nregis" name="nregis" />
-      </div>
-
-      <div class="row">
-        1 2 3 4 5 6 7 ...
-      </div>
+      </table>
 
 
     </div>
+
+    <div class="row">
+      <input type="button" value="Ver todos" />
+    </div>
+
+    <div class="row">
+      <label for="nregis">numero de registros por pagina:</label>
+    </div>
+
+    <div class="row">
+      <input type="number" id="nregis" name="nregis" />
+    </div>
+
+    <div class="row">
+      1 2 3 4 5 6 7 ...
+    </div>
+
+
+  </div>
 </template>
 
 <style scoped>
