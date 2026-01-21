e60d8f595868c3c402820ff297a3b9bedee8b12f
AARON BECERRA <aaronbecerrajuarez@gmail.com>
2026-01-21 12:21:25 -0700

    se acomodo la vista de la barra de busqueda

--- ByGUI/src/Storage.vue
+++ ByGUI/src/Storage.vue
@@ -2,48 +2,71 @@
 </script>
 
 <template>
-  <header>
+    <div class="container">
 
-    <div>
-      <form>
-        <label for="gsearch">Busqueda:</label>
-        <input type="search" id="gsearch" name="gsearch">
-      </form>
-      <br />
-      <input type="button" value="Agregar">
-      <br />
-      <hr />
-      <label for="id">ID</label>
-      &nbsp;
-      <label for="desc">DESCRIPCION</label>
-      &nbsp;
-      <label for="cant">CANTIDAD</label>
-      &nbsp;
-      <label for="img">IMAGEN</label>
-      <hr>
-      <input type="number" id="id" name="id" />
-      &nbsp;
-      <input type="text" id="desc" name="desc" />
-      &nbsp;
-      <input type="number" id="cant" name="cant" />
-      &nbsp;
-      <input type="image" />
-      &nbsp;
-      <input type="button" value="Detalles" />
-      <br />
-      <input type="button" value="Editar" />
-      &nbsp;
-      <input type="button" value="Eliminar" />
-      <hr />
-      <br />
-      <input type="button" value="Ver todos" />
-      <br />
-      <label for="nregis">numero de registros por pagina:</label>
-      <br />
-      <input type="number" id="nregis" name="nregis"/>
+      <div class="row">
+        <div class="col-6 offset-3" >
+          <div class="input-group">
+            <span class="input-group-text" id="basic-addon1"><i class="bi bi-search"></i></span>
+            <input class="form-control" type="search" placeholder="Busqueda" id="gsearch" name="gsearch">
+          </div>
+        </div>
+
+        <div class="col offset-1">
+          <input type="button" class="btn btn-primary" value="Agregar">
+        </div>
+      </div>
+
+      <div class="row">
+
+        <label for="id">ID</label>
+        &nbsp;
+        <label for="desc">DESCRIPCION</label>
+        &nbsp;
+        <label for="cant">CANTIDAD</label>
+        &nbsp;
+        <label for="img">IMAGEN</label>
+        <hr>
+
+      </div>
+
+      <div class="row">
+
+        <input type="number" id="id" name="id" />
+        &nbsp;
+        <input type="text" id="desc" name="desc" />
+        &nbsp;
+        <input type="number" id="cant" name="cant" />
+        &nbsp;
+        <input type="image" />
+        &nbsp;
+        <input type="button" value="Detalles" />
+        <br />
+        <input type="button" value="Editar" />
+        &nbsp;
+        <input type="button" value="Eliminar" />
+        <hr />
+
+      </div>
+
+      <div class="row">
+        <input type="button" value="Ver todos" />
+      </div>
+
+      <div class="row">
+        <label for="nregis">numero de registros por pagina:</label>
+      </div>
+
+      <div class="row">
+        <input type="number" id="nregis" name="nregis" />
+      </div>
+
+      <div class="row">
+        1 2 3 4 5 6 7 ...
+      </div>
+
+
     </div>
-  </header>
-
 </template>
 
 <style scoped>
