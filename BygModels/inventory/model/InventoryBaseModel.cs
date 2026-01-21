311eda0e8708708d6bf6bc07f1410d99c7758e4b
AARON BECERRA <aaronbecerrajuarez@gmail.com>
2026-01-21 16:35:51 -0700

    Se agregaron los modelos asi como todo lo necesario para que funcione el backEnd.

---
+++ BygModels/inventory/model/InventoryBaseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BygModels.inventory.model
+{
+    public class InventoryBaseModel
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ DevOpsBygController/Controllers/InventoryController.cs
@@ -0,0 +1,27 @@
+﻿using BygDevOpsManager.inventory;
+using BygModels.Inventory.dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevOpsBygController.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InventoryController : ControllerBase
+    {
+        IInventoryManager _inventoryManager;
+
+        public InventoryController(IInventoryManager inventoryManager)
+        {
+            _inventoryManager = inventoryManager;
+        }
+
+        [HttpGet(Name = "GetAllInventory")]
+
+        public async Task<IActionResult> Get() { 
+            var result = _inventoryManager.GetAll();
+            return Ok(result);
+
+        
+        }
+    }
+}
