f4edb74becbd915aad5669e3e1356aae6a85b4ca
AARON BECERRA <aaronbecerrajuarez@gmail.com>
2026-01-23 16:24:09 -0700

    se hizo la migracion de base de datos a codigo

---
+++ BygDevOpsData/Data/AppDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using BygDevOpsData.Models;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace BygDevOpsData.Data;
+
+public partial class AppDbContext : DbContext
+{
+    public AppDbContext()
+    {
+    }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<inventory> inventory { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseMySql("server=localhost;port=3306;database=BYGOPERATIONS;user=aaronbj;password=aaron123", Microsoft.EntityFrameworkCore.ServerVersion.Parse("9.6.0-mysql"));
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_0900_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<inventory>(entity =>
+        {
+            entity.HasKey(e => e.id).HasName("PRIMARY");
+
+            entity.Property(e => e.details).HasMaxLength(30);
+            entity.Property(e => e.imageurl).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ BygDevOpsData/Models/inventory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BygDevOpsData.Models;
+
+public partial class inventory
+{
+    public int id { get; set; }
+
+    public string? details { get; set; }
+
+    public int? quantity { get; set; }
+
+    public string? imageurl { get; set; }
+}
