0198d0a03fd15d542cf4e90a962e9ee6cd848cdf
AARON BECERRA <aaronbecerrajuarez@gmail.com>
2026-01-27 15:27:33 -0700

    ahora la vista es dinamica

--- ByGUI/src/storageViews/StorageCreate.vue
+++ ByGUI/src/storageViews/StorageCreate.vue
@@ -9,21 +9,35 @@
     image: string;
   }
 
+  var viewModelDescription = "";
+  var viewModelQuantity = 0;
+  var viewModelImage = "";
+
   const inventoryData = ref<InventoryModel[]>([]);
 
- /* async function getAll() {
+  /*
+  async function create() {
     try {
-      const response = await axios.get("https://localhost:44329/Inventory");
+      const response = await axios.post("https://localhost:44329/Inventory");
       inventoryData.value = response.data; // <- aquí
     } catch (error) {
       console.log(error);
     }
   }
-
-  onMounted(() => {
-    getAll();
-  });
   */
+
+  function createOnClick() {
+
+    const data: InventoryModel = {
+      id: 0,
+      description: viewModelDescription,
+      quantity: viewModelQuantity,
+      image: viewModelImage
+
+    }
+    console.log(data);
+  }
+  
 
 </script>
 
@@ -40,24 +54,24 @@
           <div class="row">
             <div class="col-12">
               <label for="descr">Descripcion:</label>
-              <input placeholder="Descripcion " class="form-control" type="text" id="descr" name="descr" />
+              <input placeholder="Descripcion " class="form-control" type="text" id="descr" name="descr" v-model="viewModelDescription" />
             </div>
           </div>
 
           <div class="row">
             <div class="col-6">
               <label for="quant">Cantidad:</label>
-              <input placeholder="Cantidad" class="form-control" type="number" id="quant" name="quant" />
+              <input placeholder="Cantidad" class="form-control" type="number" id="quant" name="quant" v-model="viewModelQuantity" />
             </div>
             <div class="col-6">
               <label for="img">Imagen:</label>
-              <input class="form-control" type="text" id="img" name="img" />
+              <input class="form-control" type="text" id="img" name="img" v-model="viewModelImage" />
             </div>
           </div>
           <br />
           <div class="row">
             <div class="col-12">
-              <input class="btn btn-primary w-100" type="submit" value="agregar" />
+              <input class="btn btn-primary w-100" type="button" value="agregar" v-on:click="createOnClick()" />
             </div>
           </div>
 
