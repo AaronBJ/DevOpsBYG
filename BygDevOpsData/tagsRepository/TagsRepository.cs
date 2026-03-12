8a9f137cfabf20232d58a3da9f96c80406455dfa
AARON BECERRA <aaronbecerrajuarez@gmail.com>
2026-03-12 14:46:03 -0700

    se creo tanto la clase como la interface del repositorio de tags

---
+++ BygDevOpsData/tagsRepository/TagsRepository.cs
@@ -0,0 +1,12 @@
+﻿using BygModels.tags;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BygDevOpsData.tagsRepository
+{
+    public class TagsRepository: ITagsRepository
+    {
+
+    }
+}
