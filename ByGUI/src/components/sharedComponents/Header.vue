17582ab447d7231dd8e5aa228beece1a3a16d5ea
AARON BECERRA <aaronbecerrajuarez@gmail.com>
2026-04-29 15:36:32 -0700

    se agrego el icono de la barra de busqueda

--- ByGUI/src/components/sharedComponents/Header.vue
+++ ByGUI/src/components/sharedComponents/Header.vue
@@ -1,19 +1,59 @@
 <template>
-  <div class="custom-header">
+  <div class="header-blue-line"></div>
 
+  <div class="header-search">
+    <img :class="{ fade: fading }"
+         :src="hover ? '/images/logoDeBusquedaVariante.svg' : '/images/logoSearch.svg'"
+         height="50"
+         @mouseenter="handleEnter"
+         @mouseleave="handleLeave" />
   </div>
 </template>
 
-<script>
-  export default {
-    name: "Home",
-  };
+<script setup lang="ts">
+  import { ref } from 'vue'
+
+  const hover = ref(false)
+  const fading = ref(false)
+
+  const handleEnter = () => {
+    fading.value = true
+    setTimeout(() => {
+      hover.value = true
+      fading.value = false
+    }, 150)
+  }
+
+  const handleLeave = () => {
+    fading.value = true
+    setTimeout(() => {
+      hover.value = false
+      fading.value = false
+    }, 150)
+  }
 </script>
 
 <style scoped>
-  .custom-header {
+  .header-search {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 70px;
+  }
+
+
+
+  .header-blue-line {
+    height: 5px;
     background-color: #204375;
-    margin-top: 10px;
+  }
+  img {
+    transition: opacity 0.3s ease;
+  }
+
+  .fade {
+    opacity: 0;
   }
 
 </style>
